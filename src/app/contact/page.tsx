2aa7950a5e82c590f929abb3c824eab4708e46e2
Richard Fisher <39341551+richfish85@users.noreply.github.com>
2025-07-14 02:15:31 +1000

    contacts update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,10 +3,10 @@ export default function ContactPage() {
     <div className="font-mono text-sm m-20">
       <h1 className="text-lg text-[var(--accent)] mb-2">📨 Contact</h1>
       <ul className="space-y-1">
-        <li>📧 staff@deepnet.com.au</li>
+        {/* <li>📧 staff@deepnet.com.au</li> */}
         <li>📧 richfish85@gmail.com</li>
-        <li>🐙 github.com/roosdy</li>
-        <li>🔗 linkedin.com/in/roosdy</li>
+        <li>🐙 <a href="https://github.com/richfish85">GitHub</a></li>
+        <li>🔗 <a href="https://www.linkedin.com/in/richard-fisher-824439177/">LinkedIn</a></li>
         <li>🔐 PGP Key Available</li>
       </ul>
     </div>
